bcac94d2f3ca706e944949ce263ad03943fba081
Orlane Vermo <orlane_v@hotmail.com>
2020-05-29 12:59:32 +0200

    Commit Init

---
+++ Assets/Scripts/Timeout.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timeout : MonoBehaviour
+{
+    public float time;
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+            SceneManager.LoadScene(0);
+    }
+}
